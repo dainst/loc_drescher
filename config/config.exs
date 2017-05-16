cb85473cb35abba20dcab169f5ba584fcafa59aa
Simon Hohl <mail@dersmon.net>
2017-05-16 16:56:34 +0200

    Added logging files.

--- config/config.exs
+++ config/config.exs
@@ -22,29 +22,20 @@ config :loc_drescher,
     subjects: "http://id.loc.gov/authorities/subjects/feed/"
   }
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+# tell logger to load a LoggerFileBackend processes
+config :logger,
+  backends: [
+    { LoggerFileBackend, :info_log },
+    { LoggerFileBackend, :error_log },
+    :console
+  ]
 
-# You can configure for your application as:
-#
-#     config :loc_drescher, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:loc_drescher, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+# configuration for the {LoggerFileBackend, :error_log} backend
+config :logger, :error_log,
+  path: "./log/info.log",
+  level: :info
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+# configuration for the {LoggerFileBackend, :error_log} backend
+config :logger, :error_log,
+  path: "./log/error.log",
+  level: :error
